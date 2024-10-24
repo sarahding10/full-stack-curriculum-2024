3e151b7c87ba24fdadbe8ccf22aa9c0ecfa373c3
Sarah <sarahding10@gmail.com>
2024-10-24 15:34:22 -0500

    week 3 homework

---
+++ Week4/create-demo/src/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MyComponent = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <h2>World</h2>
+        </div>
+    )
+}
+
+export default MyComponent;
