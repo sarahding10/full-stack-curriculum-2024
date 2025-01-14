f0ee04512836bba7828f2fe2459b11d5c8b3b3fe
Sarah <sarahding10@gmail.com>
2025-01-14 13:41:32 -0600

    functional backend

--- Week6/todo-app-backend/server.js
+++ Week6/todo-app-backend/server.js
@@ -12,9 +12,20 @@ require("dotenv").config();
 // Importing the Firestore database instance from firebase.js
 const db = require("./firebase");
 
+var admin = require("firebase-admin");
+
+if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
 // Middlewares to handle cross-origin requests and to parse the body of incoming requests to JSON
 app.use(cors());
 app.use(bodyParser.json());
+
 
 // Your API routes will go here...
 
@@ -23,7 +34,7 @@ app.get("/tasks", async (req, res) => {
   try {
     // Fetching all documents from the "tasks" collection in Firestore
     const snapshot = await db.collection("tasks").get();
-    console.log(snapshot.docs)
+    // console.log(snapshot.docs)
     let tasks = [];
     // Looping through each document and collecting data
     snapshot.forEach((doc) => {
@@ -41,6 +52,21 @@ app.get("/tasks", async (req, res) => {
   }
 });
 
+// Firebase Admin Authentication Middleware
+const auth = (req, res, next) => {
+  try {
+    const tokenId = req.get("Authorization").split("Bearer ")[1];
+    admin.auth().verifyIdToken(tokenId)
+      .then((decoded) => {
+        req.token = decoded;
+        next();
+      })
+      .catch((error) => res.status(401).send(error));
+  } catch (error) {
+    res.status(400).send("Invalid token");
+  }
+};
+
 // GET: Endpoint to retrieve all tasks for a user
 app.get('/tasks/:user', async (req, res) => {
   const user = req.params.user;
@@ -53,9 +79,23 @@ app.get('/tasks/:user', async (req, res) => {
   //res.json("hello word")
 });
 
+// app.get('/tasks/:user', async (req, res) => {
+//   try {
+//     const user = req.params.user;
+//     const tasksSnapshot = await db.collection("tasks").where("user", "==", user).get();
+//     const tasks = tasksSnapshot.docs.map((doc) => ({
+//       id: doc.id,
+//       ...doc.data(),
+//     }));
+//     res.json(tasks);
+//   } catch (error) {
+//     res.status(500).send(error.message);
+//   }
+// });
+
 const validateInput = (req, res, next) => {
-  const { user, task } = req.body;
-  if (user && task) {
+  const { user, name } = req.body;
+  if (user && name) {
     next();
   } else {
     res.status(400).json({ error: 'Incomplete task' });
@@ -66,8 +106,8 @@ const validateInput = (req, res, next) => {
 app.post('/tasks', validateInput, async (req, res) => {
   const newTask = {
     user: req.body.user,
-    task: req.body.task,
-    finished: false
+    name: req.body.name,
+    finished: req.body.finished
   };
   const taskRef = await db.collection("tasks").add(newTask);
   res.json({ id: taskRef.id, ...newTask });
